a1b16478fdbb22569319b71c1b006a10e45ebec6
Nate Mara <natemara@gmail.com>
2014-05-02 21:03:42 -0400

    added loading of data from json

--- server.py
+++ server.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
-
 import tornado.autoreload
 import tornado.ioloop
 import tornado.web
 import os
 import json
 
+pages = None
+
+def load_pages():
+	global pages
+
+	pages = {}
+	with open('pages/who.json') as json_file:
+		pages['who'] = json.load(json_file)
+
 class MainHandler(tornado.web.RequestHandler):
 	def get(self, page='index.html'):
 		page_name = safe_get(page.split('.'), 0)
+		
+		print(safe_get(pages, page_name))
+		
 		options = {}
 		self.render(page, **options)
 		
@@ -32,5 +43,6 @@ settings = {
 application = tornado.web.Application(handlers, **settings)
 
 if __name__ == '__main__':
+	load_pages()
 	application.listen(4000)
 	tornado.ioloop.IOLoop.instance().start()
